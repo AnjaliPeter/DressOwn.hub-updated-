e8a3bcb7d05adeb1fa89a9244fedc1a592aeb936
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-07-03 11:35:55 +0530

    Add files via upload

--- Frontend/src/app/signup/signup.component.ts
+++ Frontend/src/app/signup/signup.component.ts
@@ -1,1 +1,15 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
