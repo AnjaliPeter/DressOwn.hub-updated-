318141ff422545ef44a0f471ea1bd5d3a3372940
AnjaliPeter <74590192+AnjaliPeter@users.noreply.github.com>
2021-07-03 11:21:48 +0530

    Add files via upload

--- Frontend/src/app/createbar/createbar.component.ts
+++ Frontend/src/app/createbar/createbar.component.ts
@@ -1,1 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import * as $ from "jquery";
 
+
+@Component({
+  selector: 'app-createbar',
+  templateUrl: './createbar.component.html',
+  styleUrls: ['./createbar.component.css']
+})
+export class CreatebarComponent implements OnInit {
+
+  constructor(private router:Router) { }
+
+  ngOnInit(): void {
+
+  }
+
+
+  prototype(){
+    alert("alerted");
+    this.router.navigate(["/design/dresses"],{state:{"protoarea":['dresses']}});
+  }
+
+}
